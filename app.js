422fc56c05da9d09f062df6844079c308df7e5cc
Matheus Bezerra <matheusbzrr3@gmail.com>
2025-02-16 20:43:37 -0300

    finalizado

--- app.js
+++ app.js
@@ -3,7 +3,6 @@ const cors = require("cors");
 const app = express();
 
 const authRoutes = require("./routes/authRoutes");
-const userRoutes = require("./routes/userRoutes");
 
 const corsOptions = {
   origin: "http://127.0.0.1:5501",
@@ -15,7 +14,6 @@ app.use(cors(corsOptions));
 app.use(express.json());
 
 app.use("/auth", authRoutes);
-app.use("/user", userRoutes);
 
 app.get("/home", (req, res) => {
   res.status(200).json({ msg: "Bem-vindo à API!" });
