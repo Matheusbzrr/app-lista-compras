ac2fadbc820066d4ac6277102c7860f7ba257303
Matheusbzrr <154834294+Matheusbzrr@users.noreply.github.com>
2025-02-17 20:48:52 -0300

    Update app.js

--- app.js
+++ app.js
@@ -4,15 +4,18 @@ const app = express();
 
 const authRoutes = require("./src/routes/authRoutes");
 
+// CORS options
 const corsOptions = {
   origin: "https://front-gold-sigma.vercel.app",
   methods: "GET,POST,PUT,DELETE",
   allowedHeaders: "Content-Type",
 };
 
+// Aplicar o CORS antes de qualquer outra configuração
 app.use(cors(corsOptions));
 app.use(express.json());
 
+// Rotas
 app.use("/auth", authRoutes);
 
 app.get("/home", (req, res) => {
