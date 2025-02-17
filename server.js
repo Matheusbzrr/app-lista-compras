3feab9fb194589a639c36c959b04cfc300665d6c
Matheus Bezerra <matheusbzrr3@gmail.com>
2025-02-17 00:07:14 -0300

    ok

--- server.js
+++ server.js
@@ -2,6 +2,12 @@ require("dotenv").config();
 const app = require("./app.js");
 const connectDB = require("./src/config/db.js");
 
-connectDB();
+connectDB(); // Conectar ao banco de dados
+
+const PORT = process.env.PORT || 5000; // Escolher a porta de ambiente ou uma porta padrão
+
+app.listen(PORT, () => {
+  console.log(`Servidor rodando na porta ${PORT}`);
+});
 
 module.exports = app;
