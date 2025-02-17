52a4934725ae5fce4c2f129fdb46744d83b46f8b
Matheus Bezerra <matheusbzrr3@gmail.com>
2025-02-16 23:36:29 -0300

    ok

--- api/server.js
+++ api/server.js
@@ -1,6 +1,6 @@
 require("dotenv").config();
-const app = require("./app.js");
-const connectDB = require("../src/config/db.js");
+const app = require("/api/app");
+const connectDB = require("./src/config/db.js");
 
 connectDB();
 
